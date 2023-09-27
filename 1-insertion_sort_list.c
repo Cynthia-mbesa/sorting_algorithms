2b3faab0ef6efc2336d60478c051913ec987dd1f
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-09-27 15:35:20 +0300

    bubble sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - sorts a doubly linked list
+ * @list: points to the head of list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr = (*list)->next;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+
+	while (curr != NULL)
+	{
+		listint_t *temp = curr->next;
+		listint_t *prev = curr->prev;
+
+		while (prev != NULL && prev->n > curr->n)
+		{
+			if (curr->next != NULL)
+				curr->next->prev = prev;
+			if (prev->prev != NULL)
+				prev->prev->next = curr;
+
+			prev->next = curr->next;
+			curr->next = prev;
+			curr->prev = prev->prev;
+			prev->prev = curr;
+
+			if (curr->prev == NULL)
+				*list = curr;
+			print_list(*list);
+			prev = curr->prev;
+		}
+		curr = temp;
+	}
+}
