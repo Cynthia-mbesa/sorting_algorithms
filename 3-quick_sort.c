3bfef0bd9b892b88dc8bdfb14e9db1ab503e64c5
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-09-27 16:17:59 +0300

    bubble sort

---
+++ 3-quick_sort.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+/**
+ * swap - swaps two integers
+ * @a: first integer
+ * @b: second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+/**
+ * lomuto_partition - lomuto partition scheme
+ * @array: array to partition
+ * @low: starting index
+ * @high: ending index
+ * @size: size of array
+ * Return: index of pivot element
+ */
+int lomuto_partition(int *array, int low, int high, size_t size)
+{
+	int pivot = array[high];
+	int j, i = low - 1;
+
+	for (j = low; j < high; j++)
+	{
+		if (array[j] < pivot)
+		{
+			i++;
+			swap(&array[i], &array[j]);
+			print_array(array, size);
+		}
+	}
+	swap(&array[i + 1], &array[high]);
+	print_array(array, size);
+
+	return (i + 1);
+}
+/**
+ * quicksort - recursive func to perform quicksort
+ * @array: array to sort
+ * @low: starting index
+ * @high: ending index
+ * @size: size of array
+ */
+void quicksort(int *array, int low, int high, size_t size)
+{
+	if (low < high)
+	{
+		int pi = lomuto_partition(array, low, high, size);
+
+		quicksort(array, low, pi - 1, size);
+		quicksort(array, pi + 1, high, size);
+	}
+}
+/**
+ * quick_sort - sorts an array of integers
+ * @array: array to sort
+ * @size: size of array
+ */
+void quick_sort(int *array, size_t size)
+{
+	if (array == NULL || size < 2)
+		return;
+	quicksort(array, 0, size - 1, size);
+}
