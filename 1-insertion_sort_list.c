5b5bd1eb5009d6f15acd1fcae0b0d881dba61e61
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-09-27 15:49:09 +0300

    bubble sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -7,7 +7,9 @@ void insertion_sort_list(listint_t **list)
 {
 	listint_t *curr = (*list)->next;
 
-	if (list == NULL || *list == NULL)
+	if (list == NULL)
+		return;
+	if (*list == NULL)
 		return;
 
 
