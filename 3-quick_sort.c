160949f80f02d304ee16c97f82456d4531868d09
Cynthia-mbesa <cynthiambesa@gmail.com>
2023-09-27 16:27:16 +0300

    bubble sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -28,13 +28,19 @@ int lomuto_partition(int *array, int low, int high, size_t size)
 		if (array[j] < pivot)
 		{
 			i++;
-			swap(&array[i], &array[j]);
-			print_array(array, size);
+			if (i != j)
+			{
+				swap(&array[i], &array[j]);
+				print_array(array, size);
+			}
 		}
 	}
-	swap(&array[i + 1], &array[high]);
-	print_array(array, size);
-
+	if (i + 1 != high)
+	{
+		swap(&array[i + 1], &array[high]);
+		print_array(array, size);
+	}
+	
 	return (i + 1);
 }
 /**
